8a7b97fd911428c0d56af38fac4579dbf123c18f
swapnasrungarapati <swapnasnow@gmail.com>
2020-11-05 14:56:44 +0200

    shopping code send

---
+++ src/main/java/com/ssrs/shopping/ShoppingPlugin.java
@@ -0,0 +1,19 @@
+package com.ssrs.shopping;
+
+import com.ssrs.framework.extend.plugin.AbstractPlugin;
+import com.ssrs.framework.extend.plugin.PluginException;
+
+/**
+ * @author ssrs
+ */
+public class ShoppingPlugin extends AbstractPlugin {
+    @Override
+    public void start() throws PluginException {
+
+    }
+
+    @Override
+    public void stop() throws PluginException {
+
+    }
+}
